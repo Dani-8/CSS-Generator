74035713987d73927b8342a1de52375265343d1a
Dani-8 <daniyalw6190@gmail.com>
2025-08-28 00:19:15 +0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,68 @@
+// ALL NAVBAR LINK
+let boxShadowLink = document.getElementById("box-shadow");
+let textShadowLink = document.getElementById("text-shadow");
+let borderRadiusLink = document.getElementById("border-radius");
+let linearGradientLink = document.getElementById("linear-gradient");
+let animationLink = document.getElementById("animation");
+
+
+
+// ALL CONTROLS VARIABLES
+let 
+
+let boxShadowControl = document.getElementById("box-shadow-control")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
